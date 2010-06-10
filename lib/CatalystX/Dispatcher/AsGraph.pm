39893505b763f2c0a22af5267f9622c3e4724945
Franck Cuny <franck+cpan@lumberjaph.net>
2010-06-10 12:35:32 +0000

    Import of FRANCKC/CatalystX-Dispatcher-AsGraph-0.03 from CPAN.
    gitpan-cpan-distribution: CatalystX-Dispatcher-AsGraph
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         FRANCKC/CatalystX-Dispatcher-AsGraph-0.03.tar.gz
    gitpan-cpan-author:       FRANCKC
    gitpan-cpan-maturity:     released

--- lib/CatalystX/Dispatcher/AsGraph.pm
+++ lib/CatalystX/Dispatcher/AsGraph.pm
@@ -1,21 +1,39 @@
-use MooseX::Declare;
-use Graph::Easy;
-use UNIVERSAL::require;
+package CatalystX::Dispatcher::AsGraph;
 
-our $VERSION = '0.02';
+# ABSTRACT: Create a graph from Catalyst dispatcher
+
+use MooseX::Declare;
+
+our $VERSION = '0.03';
 
 class CatalystX::Dispatcher::AsGraph {
 
+    use Graph::Easy;
     with 'MooseX::Getopt';
 
-    has [qw/appname output/] => ( is => 'ro', isa => 'Str', required => 1 );
-    has 'graph' => ( is => 'ro', default => sub { Graph::Easy->new } );
+    has [qw/appname output/] => (is => 'ro', isa => 'Str', required => 1);
+
+    has graph => (
+        traits  => ['NoGetopt'],
+        is      => 'ro',
+        default => sub { Graph::Easy->new }
+    );
+    has app => (
+        traits  => ['NoGetopt'],
+        is      => 'rw',
+        isa     => 'Object',
+        lazy    => 1,
+        handles => [qw/dispatcher/],
+        default => sub {
+            my $self = shift;
+            Class::MOP::load_class($self->appname);
+            my $app = $self->appname->new;
+            $app;
+        }
+    );
 
     method run{
-        my $class  = $self->appname;
-        $class->require or die $@;
-        my $app    = $class->new;
-        my $routes = $app->dispatcher->_tree;
+        my $routes = $self->dispatcher->_tree;
         $self->_new_node($routes, '');
     }
 
@@ -36,33 +54,50 @@ class CatalystX::Dispatcher::AsGraph {
     }
 }
 
+1;
+
 
 __END__
+=pod
 
 =head1 NAME
 
 CatalystX::Dispatcher::AsGraph - Create a graph from Catalyst dispatcher
 
+=head1 VERSION
+
+version 0.03
+
 =head1 SYNOPSIS
 
     use CatalystX::Dispatcher::AsGraph;
-    my $graph = CatalystX::Dispatcher::AsGraph->new_with_options();
-    $graph->graph;
+
+    my $graph = CatalystX::Dispatcher::AsGraph->new(
+        appname => 'MyApp',
+        output  => 'myactions.png',
+    );
+    $graph->run;
 
 =head1 DESCRIPTION
 
-CatalystX::Dispatcher::AsGraph create a graph for a Catalyst application
-using his dispatcher.
+CatalystX::Dispatcher::AsGraph create a graph for a Catalyst application using his dispatcher.
 
 At the time, only private actions are graphed.
 
-=head1 AUTHOR
-
-Franck Cuny E<lt>franck@lumberjaph.netE<gt>
-
 =head1 SEE ALSO
 
-=head1 LICENSE
+L<http://www.catalystframework.org/calendar/2009/14>
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+=head1 AUTHOR
+
+  franck cuny <franck@lumberjaph.net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by franck cuny.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
